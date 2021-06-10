45be91f5aed6a2aae537369033c84eb120b80630
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-10 16:57:07 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,8 +10,8 @@ echo -e "HyperVM - Taidos Fork"
 echo -e "---------------------"
 echo -e "First Step - Install needed packages"
 echo -e "First Step - Install needed packages" >> $LOG
-rpm -Uvh https://raw.githubusercontent.com/taidos/hypervm-taidos/main/files/rpm/hypervm-core-lighttpd-1.4.54-1.el7.src.rpm >> $LOG
-rpm -Uvh https://raw.githubusercontent.com/taidos/hypervm-taidos/main/files/rpm/hypervm-core-php-5.5.38-1.el7.x86_64.rpm >> $LOG
+#rpm -Uvh https://raw.githubusercontent.com/taidos/hypervm-taidos/main/files/rpm/hypervm-core-lighttpd-1.4.54-1.el7.src.rpm >> $LOG
+#rpm -Uvh https://raw.githubusercontent.com/taidos/hypervm-taidos/main/files/rpm/hypervm-core-php-5.5.38-1.el7.x86_64.rpm >> $LOG
 yum -y install which wget zip unzip git >> $LOG
 git clone https://github.com/taidos/hypervm-taidos >> $LOG
 
