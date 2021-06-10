53e9ced90f5a67b50b99f1af7882b0e014f90a58
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-10 20:22:44 +0100

    Update installer.sh
    done for now

--- installer.sh
+++ installer.sh
@@ -10,8 +10,15 @@ echo -e "HyperVM - Taidos Fork"
 echo -e "---------------------"
 echo -e "First Step - Install needed packages"
 echo -e "First Step - Install needed packages" >> $LOG
-#rpm -Uvh https://raw.githubusercontent.com/taidos/hypervm-taidos/main/files/rpm/hypervm-core-lighttpd-1.4.54-1.el7.src.rpm >> $LOG
-#rpm -Uvh https://raw.githubusercontent.com/taidos/hypervm-taidos/main/files/rpm/hypervm-core-php-5.5.38-1.el7.x86_64.rpm >> $LOG
 yum -y install which wget zip unzip git >> $LOG
-git clone https://github.com/taidos/hypervm-taidos >> $LOG
+cd ~/
+git clone https://github.com/taidos/hypervm-taidos >> $LOG;
+rpm -Uvh ~/hypervm-taidos/files/rpm/libdb4-4.8.30-13.el7.x86_64.rpm >> $LOG;
+rpm -Uvh ~/hypervm-taidos/files/rpm/libmcrypt-2.5.8-13.el7.x86_64.rpm >> $LOG;
+rpm -Uvh ~/hypervm-taidos/files/rpm/libc-client-2007f-16.el7.x86_64.rpm >> $LOG;
+rpm -Uvh ~/hypervm-taidos/files/rpm/hypervm-core-php-5.5.38-1.el7.x86_64.rpm >> $LOG;
+rpm -Uvh ~/hypervm-taidos/files/rpm/hypervm-core-lighttpd-1.4.54-1.el7.src.rpm >> $LOG
+cd ~/hypervm-taidos/files/program-install/
+php lxins.php  --install-type=master $* | tee hypervm_install.log
+lxins.php
 
