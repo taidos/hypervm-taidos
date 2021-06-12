45b7146b901a1ff0076cdb085c951eec5d2a4d98
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-12 09:37:34 +0100

    Update lxins.php

--- files/program-install/lxins.php
+++ files/program-install/lxins.php
@@ -95,7 +95,7 @@ function lxins_main()
 
     // New since HyperVM 2.1.0 hypervm-core-php yum-plugin-replace
 #   $list = array("which", "lxlighttpd", "zip", "unzip", "hypervm-core-php", "curl", "yum-plugin-replace");
-    $list = array("which", "zip", "unzip", "curl", "yum-plugin-replace");
+    $list = array("which", "zip", "unzip", "curl", "yum-plugin-replace" , "hypervm-core-php");
 
     /* Because our builder is on CentOS-6 the binaries like closeallinput are linked against libssl.so.10
      * To keep backward compatibility with RHEL-5 / CentOS-5 systems HyperVM-NG provides openssl10 package
