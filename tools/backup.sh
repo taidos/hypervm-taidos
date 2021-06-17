de5b964c7633fc94045673a4ac85af5270a76e7b
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-17 16:51:40 +0100

    Create backup.sh
    tools

---
+++ tools/backup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+####################################
+#
+# Backup to NFS mount script. Backup entire VPS
+#
+####################################
+
+# What to backup.
+backup_files="/"
+
+# Where to backup to.
+dest="/home/backup"
+
+# Create archive filename.
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
