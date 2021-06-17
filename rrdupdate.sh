2a0da58e604f62068583b86be80528637814ecbb
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-17 16:19:50 +0100

    Update rrdupdate.sh

--- rrdupdate.sh
+++ rrdupdate.sh
@@ -3,27 +3,31 @@
 # Author:       "Brian Harrington, Alticon Inc" <bharrington@alticon.net>
 # Website:      htty://www.alticon.net
 # Editor:       "Denis Titov, ISP Okeanika" <denis@okeanika.net.ua>
-
+#TOT="1000"
+# Candidate to work as traffic monitor for the graph's
 # Time to do the data collection.
 for veid in $(/usr/sbin/vzlist -o veid -H)
 do
-        RRDFILE="/var/lib/rrd/$veid.rrd"
+        RRDFILE="/usr/local/lxlabs/hypervm/data/traffic/openvz-$veid.rrd"
         if ! test -e $RRDFILE; then
                 echo $RRDFILE does not exist, creating.
 
                 # Place your preferred RRD Creation command here
                 # or use this one, following command will create new RRD file with 300 sec step (for 5 min cron)
                 /usr/bin/rrdtool create $RRDFILE --step 300 \
-                        DS:IN:COUNTER:1000:0:U \
-                        DS:OUT:COUNTER:1000:0:U \
-                        RRA:AVERAGE:0.5:1:600000 \
-                        RRA:AVERAGE:0.5:100:60000 \
-                        RRA:AVERAGE:0.5:1000:6000
+                        DS:total:ABSOLUTE:800:-1125000000:1125000000 \
+                        DS:incoming:ABSOLUTE:800:-1125000000:1125000000 \
+                        DS:outgoing:ABSOLUTE:800:-1125000000:1125000000 \
+                        RRA:AVERAGE:0.5:1:600 \
+                        RRA:AVERAGE:0.5:6:700 \
+                        RRA:AVERAGE:0.5:24:775 \
+#                       N:$CTIN:$CTOUT \
+                        RRA:AVERAGE:0.5:288:797
         fi
         # Parse out the inbound/outbound traffic and assign them to the corresponding variables
         eval `/usr/sbin/vzctl exec $veid "grep venet0 /proc/net/dev"  |  \
                 awk -F: '{print $2}' | awk '{printf"CTIN=%s\nCTOUT=%s\n", $1, $9}'`
 
         # Send the data to the corresponding RRD time with (N)now as the update time
-        /usr/bin/rrdtool update $RRDFILE N:$CTIN:$CTOUT
+        /usr/bin/rrdtool update $RRDFILE N:$CTIN:$CTOUT:$TOT
 done
