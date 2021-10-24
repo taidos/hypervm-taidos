8e9f2ee72b7e32ce1dbf3a856ce1946114a92c31
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-10-24 07:59:18 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,14 +13,14 @@ echo -e "First Step - Install needed packages" >> $LOG
 echo -e "---------------------" >> $LOG
 yum -y install which wget zip unzip git epel-release php >> $LOG
 yum -y update >> $LOG
-yum -y install libmcrypt.x86_64 libmcrypt-devel.x86_64 >> $LOG
+yum -y install libmcrypt.x86_64 libmcrypt-devel.x86_64 libc-client.x86_64>> $LOG
 cd ~/
 git clone https://github.com/taidos/hypervm-taidos -b alpha --single-branch >> $LOG;
 cp -R ~/hypervm-taidos/files/patch/vzquota /usr/bin/vzquota >> $LOG;
 chmod 777 /usr/bin/vzquota >> $LOG;
 #rpm -Uvh ~/hypervm-taidos/files/rpm/libdb4-4.8.30-13.el7.x86_64.rpm >> $LOG;
-rpm -Uvh ~/hypervm-taidos/files/rpm/libmcrypt-2.5.8-13.el7.x86_64.rpm >> $LOG;
-rpm -Uvh ~/hypervm-taidos/files/rpm/libc-client-2007f-16.el7.x86_64.rpm >> $LOG;
+#rpm -Uvh ~/hypervm-taidos/files/rpm/libmcrypt-2.5.8-13.el7.x86_64.rpm >> $LOG;
+#rpm -Uvh ~/hypervm-taidos/files/rpm/libc-client-2007f-16.el7.x86_64.rpm >> $LOG;
 #rpm -Uvh ~/hypervm-taidos/files/rpm/hypervm-core-php-5.5.38-1.el7.x86_64.rpm >> $LOG;
 #rpm -Uvh ~/hypervm-taidos/files/rpm/hypervm-core-lighttpd-1.4.54-1.el7.src.rpm >> $LOG
 echo -e "Second Stage - Install HyperVM"
